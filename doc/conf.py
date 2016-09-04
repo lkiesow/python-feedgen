217e0842d012cb0ed50410fdd3821b0810369045
Lars Kiesow <lkiesow@uos.de>
2016-09-04 21:51:41 +0200

    Fixed TOC generation of docs
    Signed-off-by: Lars Kiesow <lkiesow@uos.de>

--- doc/conf.py
+++ doc/conf.py
@@ -40,7 +40,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'python-feedgen'
-copyright = u'2013, Lars Kiesow'
+copyright = u'2013-2016, Lars Kiesow'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
--- doc/ext/api.ext.podcast.rst
+++ doc/ext/api.ext.podcast.rst
@@ -1,5 +1,6 @@
 .. raw:: html
 
+   <script type=application/javascript src=_static/theme_extras.js></script>
    <div class="apititle"><b>Contents</b></div>
    <div class="apitoc"></div>
 
