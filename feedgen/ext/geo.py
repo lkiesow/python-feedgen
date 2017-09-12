ae5759d133251c1e5d035d95529e169a0ae41093
Bob Breznak <bob.breznak@gmail.com>
2017-09-12 15:50:05 -0400

    Better format Geo and GeoEntry. Add test for Geo Extension

--- feedgen/ext/geo.py
+++ feedgen/ext/geo.py
@@ -1,15 +1,20 @@
-from lxml import etree
+# -*- coding: utf-8 -*-
+'''
+    feedgen.ext.geo
+    ~~~~~~~~~~~~~~~~~~~
+
+    Extends the FeedGenerator to produce Simple GeoRSS feeds.
+
+    :copyright: 2017, Bob Breznak <bob.breznak@gmail.com>
+
+    :license: FreeBSD and LGPL, see license.* for more details.
+'''
+
 from feedgen.ext.base import BaseExtension
 
 class GeoExtension(BaseExtension):
-    def __init__(self):
-        self.__point__ = None
+    '''FeedGenerator extension for Simple GeoRSS.
+    '''
 
     def extend_ns(self):
         return { 'georss' : 'http://www.georss.org/georss' }
-
-    def extend_rss(self, rss_feed):
-        return rss_feed
-
-    def extend_atom(self, atom_feed):
-        return atom_feed
--- tests/test_extension.py
+++ tests/test_extension.py
@@ -134,6 +134,29 @@ class TestExtensionPodcast(unittest.TestCase):
                             namespaces=ns)
         assert author == ['Lars Kiesow']
 
+class TestExtensionGeo(unittest.TestCase):
+
+    def setUp(self):
+        self.fg = FeedGenerator()
+        self.fg.load_extension('geo')
+        self.fg.title('title')
+        self.fg.link(href='http://example.com', rel='self')
+        self.fg.description('description')
+
+    def test_geoEntryItems(self):
+        fe = self.fg.add_item()
+        fe.title('y')
+        fe.geo.point('42.36 -71.05')
+
+        assert fe.geo.point() == '42.36 -71.05'
+
+        # Check that we have the item in the resulting XML
+        ns = {'georss': 'http://www.georss.org/georss'}
+        root = etree.fromstring(self.fg.rss_str())
+        point = root.xpath('/rss/channel/item/georss:point/text()',
+                            namespaces=ns)
+        assert point == ['42.36 -71.05']
+
 
 class TestExtensionDc(unittest.TestCase):
 
